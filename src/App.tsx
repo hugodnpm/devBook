afc455352b087b404e5df44adba659d59c366740
hugodnpm <hugosc@gmail.com>
2024-01-07 12:37:08 -0300

    feat: theme style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from 'styled-components'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { GlobalStyles } from './styles/global'
 import { Search } from './pages/Search'
 import { Books } from './pages/Books/Books'
 import { BookDetail } from './pages/BookDetail/BookDetail'
+import { theme } from './styles/theme'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -15,7 +17,7 @@ function App() {
           <Route path="/books/:bookId" element={<BookDetail />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   )
 }
 
