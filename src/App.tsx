53b4caab00adbc329a33f928901cadef18693e74
hugodnpm <hugosc@gmail.com>
2024-01-10 16:32:37 -0300

    feat: add contextAPI

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import { GlobalStyles } from './styles/global'
 import { theme } from './styles/theme'
 import { SignIn } from './pages/Auth/SignIn/SignIn'
 import { SignUp } from './pages/Auth/SignUp'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/cadastro" element={<SignUp />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/cadastro" element={<SignUp />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
   )
---
+++ src/context/AuthContext/AuthContext.tsx
@@ -0,0 +1,67 @@
+import { PropsWithChildren, createContext, useState } from 'react'
+import { DEV_BOOKS_SESSION_KEY } from '../../constants/storage'
+
+interface User {
+  id: number
+  name: string
+  email: string
+}
+interface SignInUser {
+  email: string
+  password: string
+}
+interface SignUpUser {
+  name: string
+  email: string
+  password: string
+}
+
+interface Session {
+  user: User
+  accessToken: string
+  refreshToken: string
+}
+
+interface AuthContextType {
+  isAuthenticated: boolean
+  user?: User
+  signIn: (user: SignInUser) => Promise<void>
+  signUp: (user: SignUpUser) => Promise<void>
+  signOut: () => Promise<void>
+}
+
+export const AuthContext = createContext<AuthContextType>({} as AuthContextType)
+
+export function AuthProvider({ children }: PropsWithChildren) {
+  const [session, setSession] = useState<Session | null>(() => {
+    const session = localStorage.getItem(DEV_BOOKS_SESSION_KEY)
+
+    if (session) {
+      return JSON.parse(session)
+    }
+    return null
+  })
+  const signIn = async (user: SignInUser): Promise<void> => {
+    console.log(user)
+  }
+  const signUp = async (user: SignUpUser): Promise<void> => {
+    console.log(user)
+  }
+  const signOut = async (): Promise<void> => {
+    console.log('Logout')
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated: Boolean(session),
+        user: session?.user,
+        signIn,
+        signUp,
+        signOut
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
